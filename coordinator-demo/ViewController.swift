b427f15c4f2978e6379e5460b96b55dbda617858
Laurie Marceau <lmarceau@mozilla.com>
2023-03-03 12:08:37 -0500

    commit empty project

---
+++ coordinator-demo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  coordinator-demo
+//
+//  Created by Laurie Marceau on 2023-03-03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
